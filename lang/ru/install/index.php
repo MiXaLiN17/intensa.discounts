daff0ae32f757662edf35a394af21b58d8b282ba
Михаил Селиванов <mixalin17@yandex.ru>
2023-11-28 10:13:06 +0300

    переименовал модуль

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -3,5 +3,5 @@ global $MESS;
 $MESS['INTENSA_DISCOUNTS_PARTNER_NAME'] = 'Intensa';
 $MESS['INTENSA_DISCOUNTS_PARTNER_URI'] = 'https://intensa.ru/';
 
-$MESS['INTENSA_DISCOUNTS_MODULE_NAME'] = 'Intensa: Динамическое правило работы с корзиной из свойства заказа';
-$MESS['INTENSA_DISCOUNTS_MODULE_DESCRIPTION'] = 'Добавление дополнительного динамического правила работы с корзиной из свойства заказа';
+$MESS['INTENSA_DISCOUNTS_MODULE_NAME'] = 'Скидка из свойства заказа через правило работы с корзиной';
+$MESS['INTENSA_DISCOUNTS_MODULE_DESCRIPTION'] = 'Добавление дополнительного действия для правила работы с корзиной.';
