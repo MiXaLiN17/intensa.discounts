35dc901bbc4cffa9ca8df65b0e8710d94cdbf721
Михаил Селиванов <mixalin17@yandex.ru>
2023-11-28 12:37:15 +0300

    поправил дату и описание

--- install/version.php
+++ install/version.php
@@ -2,5 +2,5 @@
 
 $arModuleVersion = [
     'VERSION' => '1.0.0',
-    'VERSION_DATE' => '2023-11-24 15:00:00',
+    'VERSION_DATE' => '2023-11-28 15:00:00',
 ];
--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -4,4 +4,4 @@ $MESS['INTENSA_DISCOUNTS_PARTNER_NAME'] = 'Intensa';
 $MESS['INTENSA_DISCOUNTS_PARTNER_URI'] = 'https://intensa.ru/';
 
 $MESS['INTENSA_DISCOUNTS_MODULE_NAME'] = 'Скидка из свойства заказа через правило работы с корзиной';
-$MESS['INTENSA_DISCOUNTS_MODULE_DESCRIPTION'] = 'Добавление дополнительного действия для правила работы с корзиной.';
+$MESS['INTENSA_DISCOUNTS_MODULE_DESCRIPTION'] = 'Модуль добавляет новое действие в правила работы с корзиной: устанавливает скидку из свойства заказа, что позволяет задать динамическую величину скидки для заказа.';
